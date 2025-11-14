63e4a571f3e0d5eb26192723ab46ffd7134d3178
BenMcHughUltrasound <Ben.McHugh@warwick.ac.uk>
2025-11-14 10:43:34 +0000

    Refactored input transliteration in mpags-ciper.cpp into its own function called transformChar to declutter main.

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -3,6 +3,59 @@
 #include <string>
 #include <vector>
 
+std::string transformChar( const char in_char){
+        /*Transliterates input alphanumeric char into an uppercase string.
+
+        char in_char: char to transliterate.
+
+        return: std:string of the transliterated char. 
+        */ 
+        std::string inputText;
+    // Uppercase alphabetic characters
+        if (std::isalpha(in_char)) {
+            inputText += std::toupper(in_char);
+        
+        }
+            // Transliterate digits to English words
+        switch (in_char) {
+            case '0':
+                inputText += "ZERO";
+                break;
+            case '1':
+                inputText += "ONE";
+                break;
+            case '2':
+                inputText += "TWO";
+                break;
+            case '3':
+                inputText += "THREE";
+                break;
+            case '4':
+                inputText += "FOUR";
+                break;
+            case '5':
+                inputText += "FIVE";
+                break;
+            case '6':
+                inputText += "SIX";
+                break;
+            case '7':
+                inputText += "SEVEN";
+                break;
+            case '8':
+                inputText += "EIGHT";
+                break;
+            case '9':
+                inputText += "NINE";
+                break;
+
+        }
+    
+            // If the character isn't alphabetic or numeric, DONT add it
+        return inputText;
+}
+
+
 int main(int argc, char* argv[])
 {
     // Convert the command-line arguments into a more easily usable form
@@ -97,47 +150,8 @@ int main(int argc, char* argv[])
 
     // loop over each character from user input
     while (std::cin >> inputChar) {
-        // Uppercase alphabetic characters
-        if (std::isalpha(inputChar)) {
-            inputText += std::toupper(inputChar);
-            continue;
-        }
+        inputText += transformChar(inputChar);
 
-        // Transliterate digits to English words
-        switch (inputChar) {
-            case '0':
-                inputText += "ZERO";
-                break;
-            case '1':
-                inputText += "ONE";
-                break;
-            case '2':
-                inputText += "TWO";
-                break;
-            case '3':
-                inputText += "THREE";
-                break;
-            case '4':
-                inputText += "FOUR";
-                break;
-            case '5':
-                inputText += "FIVE";
-                break;
-            case '6':
-                inputText += "SIX";
-                break;
-            case '7':
-                inputText += "SEVEN";
-                break;
-            case '8':
-                inputText += "EIGHT";
-                break;
-            case '9':
-                inputText += "NINE";
-                break;
-        }
-
-        // If the character isn't alphabetic or numeric, DONT add it
     }
 
     // Print out the transliterated text
